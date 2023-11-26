77aa477ef5867af2ce3f3f200aed396185f46ec3
ingJcbDev <ing.jcb.dev@gmail.com>
2023-11-25 20:36:14 -0500

    Ya funciona el get

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SizeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('v1/sizes', [SizeController::class, 'index']);
+Route::post('v1/sizes', [SizeController::class, 'store']);
+Route::put('v1/sizes/{id}', [SizeController::class, 'update']);
+Route::delete('v1/sizes/{id}', [SizeController::class, 'destroy']);
+
